d4d2b31f9e70a01b13509634373f1b5cf79d93f4
ryanb3 <33189915+ryanb3@users.noreply.github.com>
2023-09-07 12:38:33 +0900

    Updated run.sh

--- run.sh
+++ run.sh
@@ -4,46 +4,39 @@
 echo "" > mcxx_outputlog
 echo "" > mcxx_errorlog
 
-# setup spack dependencies 
-spack load gcc
-spack load cmake
-spack load gperf
-spack load boost
-spack load hdf5
-spack load openmpi@3.1
+# Set up spack dependencies 
+# spack load gcc
+# spack load cmake
+# spack load gperf
+# spack load boost
+# spack load hdf5
+# spack load openmpi@3.1
 
 # Set up ccache for the build
-export CC="ccache gcc"
-export CXX="ccache g++"
+# export CC="ccache gcc"
+# export CXX="ccache g++"
 
-# Start timing
+# Start compiler build timing
 start_time=$SECONDS
 
-# config and install nanox runtime library
-cd mcxx
+# Config and install nanox runtime library
 export MERCURIUM=$PWD/mercurium
 cd nanox-0.14.1
-# Overwrite the log files if they exist
 echo "" > mcxx_outputlog
 echo "" > mcxx_errorlog
-
 autoreconf -fiv >> mcxx_outputlog 2>> mcxx_errorlog
 ./configure --prefix=$MERCURIUM >> mcxx_outputlog 2>> mcxx_errorlog
+make -j64 >> mcxx_outputlog 2>> mcxx_errorlog
+make install -j64 >> mcxx_outputlog 2>> mcxx_errorlog
 
-make -j16 >> mcxx_outputlog 2>> mcxx_errorlog
-make install -j16 >> mcxx_outputlog 2>> mcxx_errorlog
-
-# config and install mcxx compiler
+# Config and install mcxx compiler
 cd ..
-# Overwrite the log files if they exist
 echo "" > mcxx_outputlog
 echo "" > mcxx_errorlog
-
 autoreconf -fiv >> mcxx_outputlog 2>> mcxx_errorlog
 ./configure --prefix=$MERCURIUM -enable-ompss --with-nanox=$MERCURIUM >> mcxx_outputlog 2>> mcxx_errorlog
-
-make -j16 >> mcxx_outputlog 2>> mcxx_errorlog
-make install -j16 >> mcxx_outputlog 2>> mcxx_errorlog
+make -j64 >> mcxx_outputlog 2>> mcxx_errorlog
+make install -j64 >> mcxx_outputlog 2>> mcxx_errorlog
 export PATH=$MERCURIUM/bin:$PATH
 
 # End timing and calculate elapsed time
@@ -57,6 +50,6 @@ echo "--------------------"
 echo -e "BUILD TIME: \t $minutes min $seconds sec" 
 echo "--------------------"
 
-# compile and run test OpenMP program with nondeter pragma
+# Compile and run test OpenMP program with nondeter and parallelnew pragmas
 mcc -O0 -o test --openmp mytest.c >> mcxx_outputlog 2>> mcxx_errorlog
 ./test
